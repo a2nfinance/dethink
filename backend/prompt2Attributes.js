3628e7993b6e608956e9ccf9a603de6151f351d7
John <john@a2n.finance>
2023-12-02 22:00:42 +0700

    Update prompts

--- backend/prompt2Attributes.js
+++ backend/prompt2Attributes.js
@@ -8,17 +8,17 @@ const openai = new OpenAI({
 export const getAttributes = async(dps, dph, aps, dhe) => { 
   const completion = await openai.chat.completions.create({
     messages: [
-        {"role": "user", "content": `Let's create a JSON file (only JSON, no descriptions, no guides) including: 
-        ${dps}, is an integer ranging from 700 to 900, 
-        ${dph}, is a random subinterval of the interval [600, 1000], 
-        ${aps}, is a float ranging from 1 to 2 rounded to 2, 
-        ${dhe}, is a string of plus percentage that less than +30%, more than +20%.` 
+        {"role": "user", "content": `Let's create an output of JSON (no any explainations) that name-value pairs as follows:
+        ${dps}: an integer ranging from 700 to 900,
+        ${dph}: a random subinterval of the interval [600, 1000], 
+        ${aps}: a float ranging from 1 to 2 rounded to 2,
+        ${dhe}: a string of plus percentage that less than +30%, more than +20%.` 
         },
         ],
     model: "gpt-3.5-turbo",
   });
 
-  return completion.choices[0].message.content;
+  return [completion.choices[0].message.content];
 }
 
-// console.log(await getNormalWeapon("a", "b", "c", "d"));
+// console.log(await getAttributes ("Damage per Second", "Damage per Hit", "Attacks per Second", "Damage to Healthy Enemies"));
