caeb0f1eb48a346579f8e29f1b3c8a9c2927dd00
a2nfinance <levi@a2n.finance>
2023-12-09 20:33:02 +0700

    change API url

--- contracts/tasks/source/promptToAttributes.js
+++ contracts/tasks/source/promptToAttributes.js
@@ -1,24 +1,23 @@
-// const prompt = args[0]
-// const url = "https://localhost:3000";
-// const req = Functions.makeHttpRequest({
-//   url: url,
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   data: JSON.stringify({prompt: prompt})
-// });
+const prompt = args[0]
+const url = "https://dethink.a2n.finance/api/get-attributes";
+const req = Functions.makeHttpRequest({
+  url: url,
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  data: JSON.stringify({prompt: prompt})
+});
 
-// const res = await req;
-// if (res.error) {
-//     console.error(
-//         res.response
-//           ? `${res.response.status},${res.response.statusText}`
-//           : ""
-//       );
-//       throw Error("Request failed");
-// }
+const res = await req;
+if (res.error) {
+    console.error(
+        res.response
+          ? `${res.response.status},${res.response.statusText}`
+          : ""
+      );
+      throw Error("Request failed");
+}
 
-// return Functions.encodeString(JSON.stringify(res["data"]));
+return Functions.encodeString(JSON.stringify(res["data"]));
 
-return Functions.encodeString(JSON.stringify("TEST DATA"));
